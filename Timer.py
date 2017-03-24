3b95b9f95a82532efe2f4b7adf0088382c3e041f
Vibhanshu Vaibhav <vibhanshu_chaudhary@yahoo.co.in>
2017-03-24 18:39:25 +0530

    A simple timer script which takes time in seconds as input and shows a notification and plays sound when the said time has been completed.
    Libraries used:
    Notifier
    Playsound

---
+++ Timer.py
@@ -0,0 +1,21 @@
+import time as Time
+from playsound import playsound
+from pync import Notifier
+
+path = 'early-sunrise.mp3'
+
+time = input("Enter time (in seconds):")
+
+print 'Your timer has been set for' , time , 'seconds'
+
+for i in reversed(range(1,time)):
+    print 'Time left is', i , 'seconds'
+    Time.sleep(1)
+
+text =  str(time) + ' seconds has been completed'
+print text
+Notifier.notify(text, title='Timer')
+
+playsound(path, block=False)
+Time.sleep(10)
+exit()
